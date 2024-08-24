210471654dbefbb4240e47109dc707a0157c5cc2
Yosip Mike Colin <colinparrado@gmail.com>
2024-08-23 23:13:29 -0500

    Creation of interfaces

---
+++ interfaces/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  login: {
+    uuid: string;
+  };
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+}
