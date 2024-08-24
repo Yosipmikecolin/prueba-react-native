1f4312c0f33e73038e1a3e1df427c18321997761
Yosip Mike Colin <colinparrado@gmail.com>
2024-08-24 13:09:06 -0500

    Adjustment for infinite user load

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -9,13 +9,22 @@ export default function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
 
-  const getUsers = async () => {
+  const getUsers = async (pageNumber: number) => {
     try {
-      const response = await fetch("https://randomuser.me/api/?results=10");
+      setLoading(true);
+      const response = await fetch(
+        `https://randomuser.me/api/?results=5&page=${pageNumber}`
+      );
       if (response.ok) {
         const data = await response.json();
-        setUsers(data.results);
+        if (pageNumber === 1) {
+          setUsers(data.results);
+        } else {
+          setUsers((prevUsers) => [...prevUsers, ...data.results]);
+        }
       } else {
         throw new Error("Network response was not ok");
       }
@@ -23,25 +32,43 @@ export default function App() {
       setError(error.message);
     } finally {
       setLoading(false);
+      setLoadingMore(false);
     }
   };
 
   useEffect(() => {
-    getUsers();
-  }, []);
+    getUsers(page);
+  }, [page]);
+
+  const handleLoadMore = () => {
+    if (!loadingMore) {
+      setLoadingMore(true);
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
   if (error) return <Text style={{ color: "red" }}>Error: {error}</Text>;
-  if (loading) return <ActivityIndicator size="large" color="orange" />;
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <FlatList
-        data={users}
-        keyExtractor={(item) => item.login.uuid}
-        renderItem={({ item }) => <UserCard user={item} />}
-        contentContainerStyle={styles.list}
-      />
+      {loading && page === 1 ? (
+        <ActivityIndicator size="large" color="orange" />
+      ) : (
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.login.uuid}
+          renderItem={({ item }) => <UserCard user={item} />}
+          contentContainerStyle={styles.list}
+          onEndReached={handleLoadMore}
+          onEndReachedThreshold={0.2}
+          ListFooterComponent={
+            loadingMore ? (
+              <ActivityIndicator size="large" color="orange" />
+            ) : null
+          }
+        />
+      )}
     </View>
   );
 }
