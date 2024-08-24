bb15b247d90f5a39f59a4f3fe7a3eed3a1074ed2
Yosip Mike Colin <colinparrado@gmail.com>
2024-08-23 23:14:52 -0500

    Getting users from the API

--- App.tsx
+++ App.tsx
@@ -1,7 +1,33 @@
 import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { User } from "./interfaces/user";
 
 export default function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api/?results=10");
+      if (response.ok) {
+        const data = await response.json();
+        setUsers(data.results);
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
